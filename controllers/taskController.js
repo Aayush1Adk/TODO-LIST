a70edb67b5169fcd4f9de1cdc3a73c3a3fb5f9ec
Aayush Adhikari <aayush.adhikari2076@gmail.com>
2026-03-21 14:51:43 +0545

    update and delete feature

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -4,9 +4,76 @@ const Task = require("../models/Task");
 
 exports.createTask = async(req, res) => {
     try{
+        
         console.log(req.body);
         const task = await Task.create(req.body);
         res.status(201).json(task);
+    }
+    catch(error){
+        res.status(400).json({error: error.message});
+    }
+};
+
+exports.updateTask = async(req, res) => {
+    try{
+
+        const id = req.params.id;
+
+        const updateTask = await Task.findByIdAndUpdate(
+            id,
+            req.body,
+            {returnDocument: 'after'}
+        );
+
+        if(!updateTask){
+            return res.status(404).json({error:"Task not Found"});
+        }
+
+        res.json(updateTask);
+    }
+
+    catch (error){
+        res.status(400).json({error:error.message});
+    }
+
+};
+
+exports.getTasks = async(req, res) => {
+    try{
+        const tasks = await Task.find();
+        res.json(tasks);
+    }
+    catch(error){
+        res.status(400).json({error: error.message});
+    }
+};
+
+exports.getTask = async(req, res) => {
+    try{
+        const id = req.params.id;
+        const task = await Task.findById(id);
+        
+        if(!task){
+            return res.status(404).json({error: "Task not Found"});
+        }
+        
+        res.json(task);
+    }
+    catch(error){
+        res.status(400).json({error: error.message});
+    }
+};
+
+exports.deleteTask = async(req, res) => {
+    try{
+        const id = req.params.id;
+        const deletedTask = await Task.findByIdAndDelete(id);
+        
+        if(!deletedTask){
+            return res.status(404).json({error: "Task not Found"});
+        }
+        
+        res.json({message: "Task deleted successfully", deletedTask});
     }
     catch(error){
         res.status(400).json({error: error.message});
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-const { createTask} = require('../controllers/taskController');
+const { createTask, updateTask, getTasks, getTask, deleteTask } = require('../controllers/taskController');
 
 router.post('/', createTask);
+router.get('/', getTasks);
+router.get('/:id', getTask);
+router.patch('/:id', updateTask);
+router.delete('/:id', deleteTask);
 
 module.exports = router;
